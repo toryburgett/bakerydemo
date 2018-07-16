d783acbef666ef303c9ee16905616af8d03b2099
Nathan Horrigan <NathHorrigan@torchbox.com>
2018-07-16 13:55:15 +0100

    basic graphql setup

--- bakerydemo/static/frontend/gatsby-config.js
+++ bakerydemo/static/frontend/gatsby-config.js
@@ -1,5 +1,20 @@
 module.exports = {
     siteMetadata: {
-      title: `Wagtail Bakery Demo`,
-    }
-  };
+        title: `Wagtail Bakery Demo`,
+    },
+    plugins: [
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'queries',
+                path: `${__dirname}/src/queries/`,
+            },
+        },
+        {
+            resolve: 'gatsby-source-graphql',
+            options: {
+                url: 'http://localhost:8000/graphql',
+            },
+        },
+    ]
+};
--- bakerydemo/static/frontend/src/pages/index.js
+++ bakerydemo/static/frontend/src/pages/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 
-export default ({data}) => (
-  <div>
-    <h1>{data.site.siteMetadata.title}</h1>
-    <div>
-
-    </div>
-  </div>
-);
+export default ({data}) => {
+    console.log(data)
+    return (
+        <ul>
+            { data.breadGraphQl.breads.map(bread => (
+                <li>{ bread.id }</li>
+            ))}
+        </ul>
+    )
+}
 
 export const query = graphql`
-  query AboutQuery {
-    site {
-      siteMetadata {
-        title
-      }
+  query BreadQuery {
+    breadGraphQl {
+        breads {
+           id
+        }
     }
   }
 `
